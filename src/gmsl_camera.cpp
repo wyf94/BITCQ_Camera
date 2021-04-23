6013ef4be191e3fc735776dad033ff0efc12f42e
9426224 <9426224@live.com>
2021-04-23 17:30:47 +0800

    feat: 添加V4L2捕获/Rtsp捕获，待调试
    添加了V4L2的一些相关函数，调整错误信息输出，增加launch中的deviceType选择，添加RTSP获取图像信息的功能。

--- src/gmsl_camera.cpp
+++ src/gmsl_camera.cpp
@@ -1,5 +1,7 @@
 #include <gmsl_camera.h>
 
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
+
 typedef unsigned long long hash_t;
 
 constexpr hash_t prime = 0x100000001B3ull;
@@ -22,42 +24,221 @@ constexpr hash_t operator"" _hash(char const *p, size_t)
     return hash_(p);
 }
 
-//Initialize ROS node and set camera's parameters.
-void gmsl_camera::RosInit()
+//Automatical start camera init and image publish.
+int gmsl_camera::Open()
 {
-    node = ros::NodeHandle("publisher");
+    if (cameraInfo.deviceType == "gmsl" || cameraInfo.deviceType == "usb")
+    {
+        if (!OpenDevice())
+        {
+            perror("Open Device Failed!\n");
+            return -1;
+        }
+        if (!InitCamera())
+        {
+            perror("Init Camera Failed!\n");
+            return -1;
+        }
+        if (!StartCapture())
+        {
+            perror("Start Capture Failed!\n");
+            return -1;
+        }
 
-    image_transport::ImageTransport it(node);
-    pub = it.advertise("image_source", 1);
+        Spin();
 
-    node.param("device_type", cameraInfo.deviceType, std::string("other"));
-    node.param("device_address", cameraInfo.address, std::string("/dev/video0"));
-    node.param("image_width", cameraInfo.width, 640);
-    node.param("image_height", cameraInfo.height, 480);
-    node.param("fps", cameraInfo.fps, 24);
-    node.param("pixel_format", format, std::string("yuyv"));
-    cameraInfo.format = String2Enum(format);
+        StopCapture();
+        FreeBuffers();
+        CloseDevice();
+        return 1;
+    }
+    else if (cameraInfo.deviceType == "rtsp")
+    {
+        if (!RtspCamera())
+        {
+            perror("RTSP Camera Load Failed!\n");
+        }
+        return 1;
+    }
+    else if(cameraInfo.deviceType == "hik_sdk")
+    {
+        
+    }
+}
+
+//Open camera device by camera's type.
+int gmsl_camera::RtspCamera()
+{
+    cv::VideoCapture cap;
+    cv::Mat image;
+
+    cap.open(cameraInfo.address);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, cameraInfo.height);
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, cameraInfo.width);
+
+    while (node.ok())
+    {
+        if (cap.isOpened())
+        {
+            cap >> image;
+        }
+        if (image.empty())
+        {
+            printf("%s get frame failed!\n", cameraInfo.deviceType.c_str());
+            continue;
+        }
+        ProcessImage(image);
+    }
+    return 1;
+}
+
+//Open camera device by camera's type.
+int gmsl_camera::OpenDevice()
+{
+    fd = open(cameraInfo.address.c_str(), O_RDWR | O_NONBLOCK, 0);
+    if (fd < 0)
+    {
+        perror("Can't open camera.\n");
+        return -1;
+    }
+    return 1;
+}
+
+int gmsl_camera::CloseDevice()
+{
+    if (fd > 0)
+    {
+        if (close(fd) < 0)
+        {
+            perror("Can't close camera.\n");
+            return -1;
+        }
+    }
+    return 1;
+}
+
+int gmsl_camera::StartCapture()
+{
+    //Set streaming on.
+    enum v4l2_buf_type type;
+    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (ioctl(fd, VIDIOC_STREAMON, &type) < 0)
+    {
+        printf("%s start streaming failed.\n", cameraInfo.address.c_str());
+        return -1;
+    }
+    return 1;
+}
+
+int gmsl_camera::StopCapture()
+{
+    //Set streaming on.
+    enum v4l2_buf_type type;
+    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (ioctl(fd, VIDIOC_STREAMOFF, &type) < 0)
+    {
+        printf("%s stop streaming failed.\n", cameraInfo.address.c_str());
+        return -1;
+    }
+    return 1;
+}
+
+//Init buffer cache and setting camera mapping.
+int gmsl_camera::InitBuffers()
+{
+    struct v4l2_requestbuffers rb;
+    struct v4l2_buffer buf;
+    CLEAR(buf);
+    CLEAR(rb);
+
+    rb.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    rb.memory = V4L2_MEMORY_MMAP;
+    rb.count = 4; //Most usage less than 5.
+    if (ioctl(fd, VIDIOC_REQBUFS, &rb) < 0)
+    {
+        printf("%s request frame buffers failed.\n", cameraInfo.address.c_str());
+    }
+
+    if (rb.count < 2)
+    {
+        perror("Request frame buffers while insufficient buffer memory.\n");
+    }
+
+    //Calloc new memory to buffers.
+    buffers = (struct buffer *)calloc(rb.count, sizeof(*buffers));
+    if (!buffers)
+    {
+        perror("Buffer calloc out of memory.\n");
+        return -1;
+    }
+
+    //Get each buffer and mmap to userspace.
+    for (bufferCount = 0; bufferCount < rb.count; ++bufferCount)
+    {
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = bufferCount;
+
+        if (ioctl(fd, VIDIOC_QUERYBUF, &buf) < 0)
+        {
+            printf("%s does not get buffer.\n", cameraInfo.address.c_str());
+            return -1;
+        }
+        buffers[bufferCount].length = buf.length;
+        buffers[bufferCount].start = mmap(NULL /* start anywhere */,
+                                          buf.length,
+                                          PROT_READ | PROT_WRITE /* required */,
+                                          MAP_SHARED /* recommended */,
+                                          fd, buf.m.offset);
+
+        if (buffers[bufferCount].start == MAP_FAILED)
+        {
+            printf("%d buffer mmap failed.\n", bufferCount);
+            return -1;
+        }
+    }
+
+    return 1;
+}
+
+int gmsl_camera::FreeBuffers()
+{
+    unsigned int i;
+    for (i = 0; i < bufferCount; ++i)
+    {
+        if (-1 == munmap(buffers[i].start, buffers[i].length))
+        {
+            printf("munmap buffer%d failed\n", i);
+            return -1;
+        }
+    }
+    free(buffers);
+    return 0;
 }
 
 //Initialize logical camera.
-void gmsl_camera::CameraInit()
+int gmsl_camera::InitCamera()
 {
-    //Open camera device by camera's type.
-    int fd = open(cameraInfo.address.c_str(), O_RDWR | O_NONBLOCK, 0);
+    struct v4l2_capability cap;    //Find camera's capability.
+    struct v4l2_cropcap cropcap;   //Setting camera's capture mode.
+    struct v4l2_fmtdesc fmtdesc;   //Find all Camera's format：VIDIOC_ENUM_FMT
+    struct v4l2_crop crop;         //Scaling camera.
+    struct v4l2_format fmt;        //Setting camera's framerate and video parameters.
+    struct v4l2_streamparm params; //Setting Capture parameters.
 
-    //Find camera's capability.
-    struct v4l2_capability cap;
-    memset(&cap, 0, sizeof(cap));
+    CLEAR(cap);
+    CLEAR(cropcap);
+    CLEAR(fmtdesc);
+    CLEAR(crop);
+    CLEAR(fmt);
+    CLEAR(params);
+
     if (ioctl(fd, VIDIOC_QUERYCAP, &cap) < 0)
     {
         if (EINVAL == errno)
         {
-            printf("%s is no V4L2 device.\n", cameraInfo.address.c_str());
-            return;
-        }
-        else 
-        {
-            printf("Successfully Initilize Camera Device!\n");
+            printf("%s is no camera device.\n", cameraInfo.address.c_str());
+            return -1;
         }
     }
     if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE))
@@ -69,31 +250,78 @@ void gmsl_camera::CameraInit()
         printf("%s does not support streaming i/o.\n", cameraInfo.address.c_str());
     }
 
-    //Find multimedia device.
-    struct v4l2_input inp;
-    inp.index = 0;
-    int ret = ioctl(fd, VIDIOC_S_INPUT, &inp);
-
-    struct v4l2_streamparm params;
-    memset(&params, 0, sizeof(params));
     params.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     params.parm.capture.capturemode = V4L2_MODE_HIGHQUALITY;
     params.parm.capture.timeperframe.numerator = 1;
-    params.parm.capture.timeperframe.denominator = 25;
+    params.parm.capture.timeperframe.denominator = cameraInfo.fps;
+    if (ioctl(fd, VIDIOC_S_PARM, &params) < 0)
+    {
+        printf("%s can't setting capture parameters.\n", cameraInfo.address.c_str());
+    }
 
-    ret = ioctl(fd, VIDIOC_S_PARM, &params);
+    fmtdesc.index = 0;
+    fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    printf("Support format:\n");
+    while (ioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc) != -1)
+    {
+        printf("\t%d.%s\n", fmtdesc.index + 1, fmtdesc.description);
+        fmtdesc.index++;
+    }
 
-    struct v4l2_format fmt;
-    ret = ioctl(fd, VIDIOC_G_FMT, &fmt);
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    fmt.fmt.pix.width = cameraInfo.width;
+    fmt.fmt.pix.height = cameraInfo.height;
+    fmt.fmt.pix_mp.field = V4L2_FIELD_NONE;
+    fmt.fmt.pix_mp.pixelformat = V4L2_PIX_FMT_UYVY; //TO-----------------DO
+    if (ioctl(fd, VIDIOC_S_FMT, &fmt) < 0)
+    {
+        printf("%s does not support those specified parameter.\n", cameraInfo.address.c_str());
+        return -1;
+    }
+
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (ioctl(fd, VIDIOC_G_FMT, &fmt) < 0)
+    {
+        perror("Can't get frame information");
+    }
+
+    if (InitBuffers() < 0)
+    {
+        perror("Buffers init error");
+        return -1;
+    }
+
+    return 1;
 }
 
 //Get one frame from camera.
 void gmsl_camera::GetImage()
 {
-    struct v4l2_buffer buffer;
-    memset(&(buffer), 0, sizeof(buffer));
-    buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buffer.memory = V4L2_MEMORY_MMAP;
+    cv::Mat image;
+
+    struct v4l2_buffer buf;
+    CLEAR(buf);
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+
+    if (ioctl(fd, VIDIOC_DQBUF, &buf) < 0)
+    {
+        printf("%s get frame failed.\n", cameraInfo.address.c_str());
+        return;
+    }
+
+    image = cv::Mat(cv::Size(cameraInfo.width, cameraInfo.height), CV_8UC3, buffers[buf.index].start);
+
+    ProcessImage(image);
+}
+
+//Process Image which grab from memory.
+void gmsl_camera::ProcessImage(cv::Mat image)
+{
+    cv::cvtColor(image, image, cv::COLOR_YUV420p2BGR);
+
+    msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+    pub.publish(msg);
 }
 
 //Loop detection of image acquisition and image sending.
